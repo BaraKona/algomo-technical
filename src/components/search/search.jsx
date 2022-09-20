48b73294fadabe40153278ae14220e5c846ddf82
Bara Konateh <62246274+BaraKona@users.noreply.github.com>
2022-09-20 21:40:51 +0100

    Design finalisation

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -5,6 +5,7 @@ import './search.scss'
 function Search() {
   const [submitDisabled, setSubmitDisabled] = useState(true)
   const [marvelCharacters, setMarvelCharacters] = useState([])
+  const [showList, setShowList] = useState('')
   const [error, setError] = useState('')
 
   const searching = async (searchInput) => {
@@ -13,31 +14,32 @@ function Search() {
       await getCharacters(searchInput)
     } else {
       setSubmitDisabled(true)
+      setMarvelCharacters([])
+      setShowList('')
     }
   }
   const getCharacters = async (searchInput) => {
     axios.get('https://gateway.marvel.com/v1/public/characters', {
-      params: { nameStartsWith: searchInput, apikey: '23ad8ef10fd6155e9351a458df23faf5'}
+      params: { nameStartsWith: searchInput, apikey: process.env.REACT_APP_PUBLIC_MARVEL_KEY}
     })
     .then((data) => {
-      console.log(data.data.data.results)
       setMarvelCharacters(data.data.data.results)
-      console.log(marvelCharacters)
+      setShowList('active')
     }).catch((error) => {
-      console.log(error)
       setError('Something went wrong.... refresh the page and try again')
     })
   }
   return (
     <>
     <form className="search">
-      <input className="search__input" list="marvelCharacters" placeholder="Search terms" onChange={(e) => {searching(e.target?.value)}}/>
-      <datalist id="marvelCharacters">
+      <label className="label"> Search </label>
+      <input className="search__input" placeholder="Search terms" onChange={(e) => {searching(e.target?.value)}}/>
+      <div id="marvelCharacters" className={`datalist ${showList}`}>
         {marvelCharacters?.map((character, index) => {
-          return <option value={character?.name} key={index} />
+          return <p className="option" key={index}>{character?.name}</p>
         })}
-      </datalist>
-      <input className="search__submit" type="submit" onSubmit={(e) => {getCharacters(e)}} disabled={submitDisabled} value="SEARCH"></input>
+      </div>
+      <input className="search__submit" type="submit" onSubmit={(e) => {getCharacters(e)}} disabled={submitDisabled} value="SEARCH"/>
     </form>
     <p className="error">{error}</p>
     </>
